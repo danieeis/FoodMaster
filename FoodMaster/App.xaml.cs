f9f1f77dc0b370b9c8bf103555455904a6d5695b
Daniel Tovar <danieldaniyyelda@gmail.com>
2021-09-28 03:09:16 -0400

    Create app
    Signed-off-by: Daniel Tovar <danieldaniyyelda@gmail.com>

---
+++ FoodMaster/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FoodMaster.Services;
+using FoodMaster.Views;
+
+namespace FoodMaster
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FoodMaster/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FoodMaster.ViewModels;
+using FoodMaster.Views;
+using Xamarin.Forms;
+
+namespace FoodMaster
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
