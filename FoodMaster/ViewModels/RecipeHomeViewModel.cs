fa6172919e3af2219e6f0a80333fbcfae172908c
Daniel Tovar <danieldaniyyelda@gmail.com>
2021-10-08 03:20:59 -0400

    add home first frame

--- FoodMaster/ViewModels/RecipeHomeViewModel.cs
+++ FoodMaster/ViewModels/RecipeHomeViewModel.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using FoodMaster.Services;
+using FoodMaster.Views;
+using System;
+using Xamarin.Forms;
+
 namespace FoodMaster.ViewModels
 {
     public class RecipeHomeViewModel : BaseViewModel
     {
+
+        public Command LogoutCommand { get; }
+        UserService _userService;
         public RecipeHomeViewModel()
         {
             Title = "Home";
+            _userService = DependencyService.Get<UserService>();
+            LogoutCommand = new Command(Logout);
+        }
+
+        private void Logout(object obj)
+        {
+            _userService.InvalidateToken();
+            App.Current.MainPage = new LoginPage();
         }
     }
 }
